b9ce6f66c9101505d09d27aae3ae2358352733c2
DataFire <info@datafire.io>
2020-01-30 15:32:06 +1100

    changed filter to last modified

--- actions/get_orders.js
+++ actions/get_orders.js
@@ -5,7 +5,8 @@ let ebay_sell_fulfillment = require('@datafire/ebay_sell_fulfillment').actions;
 module.exports = new datafire.Action({
   inputs: [{
     type: "string",
-    title: "lastUpdated"
+    title: "lastUpdated",
+    default: "lastmodifieddate:[2020-01-01T00:00:00.001Z..]"
   }],
   handler: async (input, context) => {
     let orderSearchPagedCollection = await ebay_sell_fulfillment.getOrders({
