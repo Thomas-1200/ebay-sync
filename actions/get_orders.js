7605e6a2ccde29810d3974524dc4aafa5c850afb
DataFire <info@datafire.io>
2020-02-01 13:50:25 +1100

    added customer data collection

--- actions/get_orders.js
+++ actions/get_orders.js
@@ -11,6 +11,10 @@ let added = 0;
 let ordersFound = 0;
 let needsUpdating = [];
 let needsInserting = [];
+let newCustomers = [];
+let existingCustomers = [];
+let newCustomersAdded = 0;
+let existingCustomersUpdated = 0;
 
 let ebay_sell_fulfillment = require('@datafire/ebay_sell_fulfillment').actions;
 module.exports = new datafire.Action({
@@ -21,15 +25,33 @@ module.exports = new datafire.Action({
     let client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     await client.connect() 
     let collection = await client.db("itsyourdayboutique").collection("orders");
+    let customerCollection = await client.db("itsyourdayboutique").collection("customers");
     ordersFound = orderSearchPagedCollection.orders.length;
     for(let i = 0; i < orderSearchPagedCollection.orders.length; i++) {
+      
       let item = orderSearchPagedCollection.orders[i];
+      let customer = {
+        username: item.buyer.username,
+        fullname: item.fulfillmentStartInstructions[0].shippingStep.shipTo.fullName || "",
+        contact: {
+        	email: item.fulfillmentStartInstructions[0].shippingStep.shipTo.email || "",
+          	phone: item.fulfillmentStartInstructions[0].shippingStep.shipTo.primaryPhone.phoneNumber || "",
+          	address: item.fulfillmentStartInstructions[0].shippingStep.shipTo.contactAddress || {}
+        },
+        orders: [item.orderId]
+      }
       let found = await collection.findOne({orderId: item.orderId});
-       
+      let customerFound = await customerCollection.findOne({username: customer.username});
       if (found && found.orderId === item.orderId) {
         needsUpdating.push(item);
       } else {
       	needsInserting.push(item);
+      }
+      
+      if (customerFound && customerFound.username === customer.username) {
+       	existingCustomers.push([customer, customerFound]);
+      } else {
+      	newCustomers.push(customer);
       }
     }
     
@@ -43,7 +65,34 @@ module.exports = new datafire.Action({
         added++;
     }
     
-    message = "orders found: " + ordersFound + ", orders updated: " + updated + ", orders inserted: " + added;
+    for (let i = 0; i < existingCustomers.length; i++) {
+    	let existingRecord = existingCustomers[i][1];
+      	let newRecord = existingCustomers[i][0];
+      
+      	if (existingRecord.fullname.length > 0 && newRecord.fullname.length < 1) {
+        	newRecord.fullname = existingRecord.fullname;
+        }
+      	if (existingRecord.contact.email.length > 0 && newRecord.contact.email.length < 1) {
+        	newRecord.contact.email = existingRecord.contact.email;
+        }
+      	if (existingRecord.contact.phone.length > 0 && newRecord.contact.phone.length < 1) {
+        	newRecord.contact.phone = existingRecord.contact.phone;
+        }
+      	if (existingRecord.contact.address.addressLine1.length > 0 && newRecord.contact.address.addressLine1.length < 1) {
+        	newRecord.contact.address = existingRecord.contact.address;
+        }
+      
+      	newRecord.orders.push(...existingRecord.orders);
+      	await customerCollection.replaceOne({username:newRecord.username}, newRecord);
+      	existingCustomersUpdated++
+    }
+    
+    for (let i = 0; i < newCustomers.length; i++) {
+    	await customerCollection.insertOne(newCustomers[i]);
+      	newCustomersAdded++
+    }
+    
+    message = "orders found: " + ordersFound + ", orders updated: " + updated + ", orders inserted: " + added + ", customers updated: " + existingCustomersUpdated + ", customers added: " + newCustomersAdded;
     return message;
     },
 });
