d469ac5ee4b0cc865b33927c51ee25024a6ffe5a
DataFire <info@datafire.io>
2020-02-03 19:34:24 +1100

    dded path to return orders

--- actions/test.js
+++ actions/test.js
@@ -1,12 +1,13 @@
 "use strict";
 let datafire = require('datafire');
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://iyd-admin:tcQrNRIkWvxz5dPp4LWp0PdRhTZAn5lx1KSeDtkR42DDMkwppZGsJdj3IuI1gvrl@cluster0-hcq6x.mongodb.net";
+let client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let ebay_sell_fulfillment = require('@datafire/ebay_sell_fulfillment').actions;
 module.exports = new datafire.Action({
   handler: async (input, context) => {
-    let orderSearchPagedCollection = await ebay_sell_fulfillment.getOrders({
-      limit: '1000',
-    }, context);
-    return orderSearchPagedCollection.orders.length;
+    await client.connect();
+    let collection = await client.db("itsyourdayboutique").collection("orders");
+    return collection.find({}).toArray();
   },
 });
